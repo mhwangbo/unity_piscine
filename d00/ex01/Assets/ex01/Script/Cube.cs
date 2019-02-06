855108c4a916772b3f679c1491fa3654e0925904
mhwangbo <mhwangbo90@gmail.com>
2019-02-05 16:38:37 -0800

    to ex01 - quick time event

---
+++ d00/ex01/Assets/ex01/Script/Cube.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    private float speed;
+
+    void Start() {
+      speed = Random.Range(2f, 4f);
+    }
+    void Update()
+    {
+      float precision = transform.position.y + 4;
+
+      transform.Translate(Vector3.down * speed * Time.deltaTime);
+      if (transform.position.y < -4.5)
+      {
+        if (tag == "a")
+          CubeSpawner.letter[0] = 0;
+        else if (tag == "s")
+          CubeSpawner.letter[1] = 0;
+        else if (tag == "d")
+          CubeSpawner.letter[2] = 0;
+        Destroy(transform.gameObject);
+      }
+      else if (tag == "a" && Input.GetKeyDown("a") && transform.position.y >= -4)
+      {
+        Debug.Log("Precision: " + precision);
+        CubeSpawner.letter[0] = 0;
+        Destroy(transform.gameObject);
+      }
+      else if (tag == "s" && Input.GetKeyDown("s") && transform.position.y >= -4)
+      {
+        Debug.Log("Precision: " + precision);
+        CubeSpawner.letter[1] = 0;
+        Destroy(transform.gameObject);
+      }
+      else if (tag == "d" && Input.GetKeyDown("d") && transform.position.y >= -4)
+      {
+        Debug.Log("Precision: " + precision);
+        CubeSpawner.letter[2] = 0;
+        Destroy(transform.gameObject);
+      }
+    }
+}
---
+++ d00/ex01/Assets/ex01/Script/CubeSpawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeSpawner : MonoBehaviour
+{
+  public GameObject[] cubePrefabs;
+  private float timer = 0.0f;
+  private float waitTime = 0.5f;
+  public static int[] letter = new int[3];
+
+  void Start() {
+    letter[0] = 0;
+    letter[1] = 0;
+    letter[2] = 0;
+  }
+
+    void  Update()
+  {
+    timer += Time.deltaTime;
+    if (timer > waitTime ) {
+      timer -= waitTime;
+      int random = Random.Range(0, 3);
+      if (letter[random] == 0)
+      {
+            GameObject.Instantiate(cubePrefabs[random]);
+            letter[random] = 1;
+      }
+
+    }
+  }
+}
