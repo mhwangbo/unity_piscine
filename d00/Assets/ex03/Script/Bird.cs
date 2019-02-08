4cd017d96cf7fab101bbe824ebdfe3b5dd67f8d7
mhwangbo <mhwangbo90@gmail.com>
2019-02-07 16:59:00 -0800

    changed to one project

---
+++ d00/Assets/ex03/Script/Bird.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    public float maxHeight;
+    public float jumpHeight;
+    public float dropRate;
+    public int score;
+    public float increment;
+    public bool isDead = false;
+
+    void Update()
+    {
+      if (transform.position.y < -2.27 || transform.position.y > 5)
+        isDead = true;
+      if (!isDead)
+      {
+        transform.Translate(Vector3.down * Time.deltaTime * dropRate);
+        if (score % 30 == 0)
+          increment += 0.1f;
+        if (Input.GetKeyDown("space"))
+        {
+          transform.Translate(Vector3.up * Time.deltaTime * jumpHeight);
+        }
+      }
+      else
+      {
+        Debug.Log("Score: " + score + "\nTime: " + Mathf.RoundToInt(Time.time) + "s");
+        Destroy(gameObject);
+      }
+    }
+}
---
+++ d00/Assets/ex03/Script/Pipe.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pipe : MonoBehaviour
+{
+    static private float speed = 1.0f;
+    public Bird bird;
+    bool scored;
+
+    void Update()
+    {
+        if (!bird.isDead) {
+            transform.Translate(Vector3.left * (speed + bird.increment) * Time.deltaTime);
+            if (transform.position.x < -8.0f)
+            {
+              transform.position = new Vector3(8.5f, transform.position.y, transform.position.z);
+              scored = false;
+            }
+            if (transform.position.x > -5.0f && transform.position.x < -4.0f)
+            {
+              if (bird.transform.position.y > 3.23f || bird.transform.position.y < 0.36f)
+              {
+                if (scored)
+                  bird.score -= 5;
+                bird.isDead = true;
+              }
+              else if (!scored)
+              {
+                bird.score += 5;
+                scored = true;
+              }
+            }
+        }
+    }
+}
