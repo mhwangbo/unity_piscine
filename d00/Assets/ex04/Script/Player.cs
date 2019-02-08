7b8ae7ae6eec388278b097d5dc42a0b7d221c89b
mhwangbo <mhwangbo90@gmail.com>
2019-02-07 23:30:33 -0800

    d00 finished

--- d00/Assets/ex04/Script/Player.cs
+++ d00/Assets/ex04/Script/Player.cs
@@ -4,15 +4,52 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public int player_number;
+    public PongBall ball;
+    public int score;
+    public Player opponent;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (player_number == 1)
+        {
+            if (Input.GetKey("w") && transform.position.y + 2 <= 5)
+            {
+                transform.Translate(Vector3.up * Time.deltaTime * 5);
+            }
+            else if (Input.GetKey("s") && transform.position.y - 2 >= -5)
+            {
+                transform.Translate(Vector3.down * Time.deltaTime * 5);
+            }
+        }
+        else if (player_number == 2)
+        {
+            if (Input.GetKey("up") && transform.position.y + 2 <= 5)
+            {
+                transform.Translate(Vector3.up * Time.deltaTime * 5);
+            }
+            else if (Input.GetKey("down") && transform.position.y - 2 >= -5)
+            {
+                transform.Translate(Vector3.down * Time.deltaTime * 5);
+            }
+        }
+        if (player_number == 1 && ball.transform.position.x <= transform.position.x + 0.5 ||
+                    player_number == 2 && ball.transform.position.x >= transform.position.x - 0.5)
+        {
+            if (ball.transform.position.y >= transform.position.y - 2 &&
+                     ball.transform.position.y <= transform.position.y + 2)
+            {
+                ball.hit = true;
+            }
+            else if (!ball.dead)
+            {
+                ball.dead = true;
+                opponent.score++;
+                if (player_number == 2)
+                    ball.direction = 1;
+                else
+                    ball.direction = 0;
+            }
+        }
     }
 }
--- d00/Assets/ex04/Script/PongBall.cs
+++ d00/Assets/ex04/Script/PongBall.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class PongBall : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public bool hit;
+    public int direction = 0;
+    private Vector3 angle;
+    private bool gameStart = false;
+    public Player playerOne;
+    public Player playerTwo;
+    public bool dead;
+
+    void Begin()
     {
-        
+        transform.position = new Vector3(0, 0, 0);
+        if (direction == 0)
+            {
+                angle = new Vector3(-7.5f, Random.Range(-4.5f, 4.5f), 0);
+
+            } else
+            {
+                angle = new Vector3(7.5f, Random.Range(-4.5f, 4.5f), 0);
+            }
+            gameStart = true;
+            if (dead)
+                Debug.Log("Player 1: " + playerOne.score + " | Player 2: " + playerTwo.score);
+            dead = false;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (!gameStart || dead)
+            Begin();
+        if (hit)
+        {
+            angle.x *= -1;
+            angle.y *= -1;
+            transform.Translate(angle * Time.deltaTime * 2);
+            hit = false;
+        }
+        else if (!hit && !dead && gameStart)
+        {
+            if (transform.position.y > 4.5 || transform.position.y < -4.5)
+            {
+                angle.y *= -1;
+            }
+            transform.Translate(angle * Time.deltaTime);
+        }
     }
 }
