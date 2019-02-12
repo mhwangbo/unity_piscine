3c1c07d8c485e3b7d449ce2990ec96b1e6be0b3e
mhwangbo <mhwangbo90@gmail.com>
2019-02-12 15:52:25 -0800

    working on non-update loop solution

---
+++ d02/Assets/Script/footman.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class footman : MonoBehaviour
+{
+    public float speed = 1.5f;
+    private Vector3 target;
+
+    public Animator anim;
+    public bool selected = false;
+    private bool isRight = true;
+    private float dirX;
+    private float dirY;
+
+    void Start()
+    {
+        target = transform.position;
+        anim = GetComponent<Animator>();
+    }
+
+    public void WhenSelected()
+    {
+        if (selected)
+            InvokeRepeating("Move", 0, 0.1f);
+        else if (!selected)
+            CancelInvoke();
+    }
+
+    void Move()
+    {
+           if (Input.GetMouseButtonDown(0))
+            {
+                target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                target.z = transform.position.z;
+                dirX = target.x - transform.position.x;
+                dirY = target.y - transform.position.y;
+                anim.SetBool("walk", true);
+                anim.SetFloat("x", target.x - transform.position.x);
+                anim.SetFloat("y", target.y - transform.position.y);
+                flip();
+            }
+            transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+            if (transform.position == target)
+            {
+                anim.SetBool("walk", false);
+            }
+    }
+
+    void flip()
+    {
+        int sign = 1;
+        if (isRight && dirX <= 0)
+        {
+            sign = -1;
+            isRight = false;
+        }
+        else if (!isRight && dirX > 0)
+        {
+            sign = -1;
+            isRight = true;
+        }
+        Vector3 temp = anim.transform.localScale;
+        temp.x *= sign;
+        anim.transform.localScale = temp;
+    }
+}
---
+++ d02/Assets/Script/footmanController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class footmanController : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(worldPoint, Vector2.zero);
+
+            if (hit.collider.tag == "footman")
+            {
+                footman fm = hit.collider.gameObject.GetComponent<footman>();
+                fm.selected = true;
+                fm.WhenSelected();
+            }
+        }
+    }
+}
