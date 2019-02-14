0cf095ba9422c2d1aeca8eb6fe165ef40bbd9252
mhwangbo <mhwangbo90@gmail.com>
2019-02-14 03:52:39 -0800

    title screen

---
+++ d03/Assets/Script/TitleController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleController : MonoBehaviour
+{
+    public void OnStartButtonClicked()
+    {
+        SceneManager.LoadScene("ex01");
+    }
+
+    public void OnExitButtonClicked()
+    {
+        Application.Quit();
+    }
+}
