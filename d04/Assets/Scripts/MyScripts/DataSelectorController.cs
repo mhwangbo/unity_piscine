4838098e75c024f82c415322a9b000a375f6a51c
mhwangbo <mhwangbo90@gmail.com>
2019-02-18 13:37:05 -0800

    working on stage1, data selector completed

---
+++ d04/Assets/Scripts/MyScripts/DataSelectorController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class DataSelectorController : MonoBehaviour
+{
+    private Text lifeLost;
+    private Text bestScore;
+
+    private int modernLevel;
+    private int metalLevel;
+    private int dubstepLevel;
+
+    public GameObject[] modernRoute;
+    public GameObject[] metalRoute;
+    public GameObject[] dubstepRoute;
+
+    public GameObject selector;
+
+    private GameObject[][] allRoute = new GameObject[3][];
+    private int i = 0;
+    private int j = 0;
+
+    void Start()
+    {
+        allRoute[0] = modernRoute;
+        allRoute[1] = metalRoute;
+        allRoute[2] = dubstepRoute;
+
+        lifeLost = GameObject.Find("LifeLostText").GetComponent<UnityEngine.UI.Text>();
+        bestScore = GameObject.Find("BestScoreText").GetComponent<UnityEngine.UI.Text>();
+        lifeLost.text = "" + PlayerPrefs.GetInt("lostLife");
+        bestScore.text = "" + PlayerPrefs.GetInt("bestScore");
+
+        modernLevel = PlayerPrefs.GetInt("modernLevel");
+        metalLevel = PlayerPrefs.GetInt("metalLevel");
+        dubstepLevel = PlayerPrefs.GetInt("dubstepLevel");
+
+        for (int i = modernLevel + 1; i < 4; i++)
+            modernRoute[i].SetActive(false);
+        for (int i = metalLevel + 1; i < 4; i++)
+            metalRoute[i].SetActive(false);
+        for (int i = dubstepLevel + 1; i < 4; i++)
+            dubstepRoute[i].SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            if (Input.GetKeyDown("right"))
+                i += (i < 3 ? 1 : -3);
+            if (Input.GetKeyDown("left"))
+                i -= (i > 0 ? 1 : -3);
+            if (Input.GetKeyDown("up"))
+                j -= (j > 0 ? 1 : -2);
+            if (Input.GetKeyDown("down"))
+                j += (j < 2 ? 1 : -2);
+            selector.transform.position = allRoute[j][i].transform.position;
+            if (allRoute[j][i].activeInHierarchy && Input.GetKeyDown("return"))
+            {
+                string stageName = allRoute[j][i].transform.name;
+                SceneManager.LoadScene(stageName);
+            }
+        }
+    }
+}
---
+++ d04/Assets/Scripts/MyScripts/TitleController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TitleController : MonoBehaviour
+{
+    public Text flashingText;
+    public GameObject ResetText;
+
+    void Start()
+    {
+        StartCoroutine(BlinkText());
+    }
+
+    public IEnumerator BlinkText()
+    {
+        while (true)
+        {
+            flashingText.text = "press enter to play";
+            yield return new WaitForSeconds(.5f);
+            flashingText.text = "";
+            yield return new WaitForSeconds(.5f);
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("return"))
+        {
+            SceneManager.LoadScene("DataSelect");
+        }
+    }
+
+    public void ResetButton()
+    {
+        PlayerPrefs.SetInt("lostLife", 0);
+        PlayerPrefs.SetInt("bestScore", 0);
+        PlayerPrefs.SetInt("modernLevel", 0);
+        PlayerPrefs.SetInt("metalLevel", 0);
+        PlayerPrefs.SetInt("dubstepLevel", 0);
+        ResetText.SetActive(true);
+    }
+}
