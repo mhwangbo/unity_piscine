997eb8e8183b5393fb2c5e34d87c6faab2ce56a1
mhwangbo <mhwangbo90@gmail.com>
2019-03-06 14:56:29 -0800

    level design without major scripting

---
+++ d06/Assets/Scripts/LightController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightController : MonoBehaviour
+{
+    float rotationLeft = 360;
+    float rotationSpeed = 30;
+    void Update()
+    {
+
+        float rotation = rotationSpeed * Time.deltaTime;
+        if (rotationLeft > rotation)
+        {
+            rotationLeft -= rotation;
+        }
+        else
+        {
+            rotation = rotationLeft;
+            rotationLeft = 0;
+        }
+        transform.Rotate(0, rotation, 0);
+    }
+}
