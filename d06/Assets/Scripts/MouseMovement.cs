3542efdfbf56ba4b1ff59506a8a2ee8e5541c65f
mhwangbo <mhwangbo90@gmail.com>
2019-03-06 19:59:10 -0800

    up down movement restricted

--- d06/Assets/Scripts/MouseMovement.cs
+++ d06/Assets/Scripts/MouseMovement.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class MouseMovement : MonoBehaviour
 {
-    public float speedH = 20.0f;
-    public float speedV = 20.0f;
+    public float speedH = 15.0f;
+    public float speedV = 15.0f;
     private float yaw = 0.0f;
     private float pitch = 0.0f;
 
     void Update()
     {
+        float tmp = pitch - (speedV * Input.GetAxis("Mouse Y"));
+        if (tmp >= -20.0f && tmp <= 20.0f)
+            pitch = tmp;
         yaw += speedH * Input.GetAxis("Mouse X");
-        pitch -= speedV * Input.GetAxis("Mouse Y");
         transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
     }
 }
