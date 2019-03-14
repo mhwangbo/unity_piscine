d5d3fb2760dac49c0615426379748d3c510aef9d
mhwangbo <mhwangbo90@gmail.com>
2019-03-14 15:20:05 -0700

    Camera movement and character movement on click

---
+++ d08/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    private Animator animator;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
+            {
+                agent.isStopped = false;
+                agent.destination = hit.point;
+                animator.SetBool("running", true);
+            }
+        }
+        if (!agent.pathPending)
+        {
+            if (agent.remainingDistance <= agent.stoppingDistance)
+            {
+                if (!agent.hasPath || agent.velocity.sqrMagnitude == 0.0f)
+                {
+                    agent.isStopped = true;
+                    animator.SetBool("running", false);
+                }
+            }
+        }
+    }
+}
