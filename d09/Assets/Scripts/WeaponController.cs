c782b8c7cf20518a5808b906bc53344bb6eef731
mhwangbo <mhwangbo90@gmail.com>
2019-03-15 21:07:24 -0700

    d09 initiated

---
+++ d09/Assets/Scripts/WeaponController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class WeaponController : MonoBehaviour
+{
+    // Stats
+    [SerializeField] private bool areaDamage;
+    [SerializeField] private float range;
+    [SerializeField] private float attackSpeed;
+    [SerializeField] private float damage;
+
+    // Visual and Audio
+    [SerializeField] private AudioSource shotAudio;
+    [SerializeField] private GameObject shotPoint;
+    [SerializeField] private GameObject trail;
+    [SerializeField] private MeshRenderer mesh;
+    [SerializeField] private SkinnedMeshRenderer skinnedMesh;
+    [SerializeField] private Animator animator;
+
+    // getter
+    public bool AreaDamage { get { return areaDamage; } }
+    public float Range { get { return range; } }
+    public float AttackSpeed { get { return attackSpeed; } }
+    public float Damage { get { return damage; } }
+    public AudioSource ShotAudio { get { return shotAudio; } }
+    public GameObject ShotPoint { get { return shotPoint; } }
+    public GameObject Trail { get { return trail; } }
+
+    public MeshRenderer Mesh { get { return mesh; } set { mesh.enabled = value; } }
+    public SkinnedMeshRenderer SkinnedMesh { get { return skinnedMesh; } set { skinnedMesh.enabled = value; } }
+
+    public void TriggerAnimation()
+    {
+        animator.SetTrigger("shot");
+    }
+}
+
+//[CreateAssetMenu(fileName = "New WeaponData", menuName = "Weapon Data", order = 51)]
+//public class WeaponData : ScriptableObject
+//{
+//    // Stats
+//    [SerializeField] private bool areaDamage;
+//    [SerializeField] private float range;
+//    [SerializeField] private float attackSpeed;
+//    [SerializeField] private float damage;
+
+//    // Visual and Audio
+//    [SerializeField] private AudioSource shotAudio;
+//    [SerializeField] private GameObject shotPoint;
+//    [SerializeField] private GameObject trail;
+//    [SerializeField] private MeshRenderer mesh;
+//    [SerializeField] private SkinnedMeshRenderer skinnedMesh;
+
+//    // getter
+//    public bool AreaDamage { get { return areaDamage; } }
+//    public float Range { get { return range; } }
+//    public float AttackSpeed { get { return attackSpeed; } }
+//    public float Damage { get { return damage; } }
+//    public AudioSource ShotAudio { get { return shotAudio; } }
+//    public GameObject ShotPoint { get { return shotPoint; } }
+//    public GameObject Trail { get { return trail; } }
+
+//    public MeshRenderer Mesh { get { return mesh; } set { mesh.enabled = value; } }
+//    public SkinnedMeshRenderer SkinnedMesh { get { return skinnedMesh; } set { skinnedMesh.enabled = value; } }
+//}
