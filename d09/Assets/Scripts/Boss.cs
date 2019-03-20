a16bb51385eb83cb6bb9b6328f31328563d7503b
mhwangbo <mhwangbo90@gmail.com>
2019-03-19 21:38:59 -0700

    d09 working on boss

---
+++ d09/Assets/Scripts/Boss.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss : MonoBehaviour
+{
+    BasicEnemy basicEnemy;
+
+    void Start()
+    {
+        basicEnemy = GetComponent<BasicEnemy>();
+    }
+
+    void Update()
+    {
+        if(!basicEnemy.IsKilled)
+        {
+
+        }
+    }
+}
